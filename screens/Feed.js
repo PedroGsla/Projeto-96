0a3c8e54ccd4de4d8cf0c978440ca04a203ce6c8
PedroGsla <111892198+PedroGsla@users.noreply.github.com>
2023-09-28 11:38:43 -0300

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image
+} from "react-native";
+
+
+import PostCard from "./PostCard";
+import { FlatList } from "react-native-gesture-handler";
+
+let posts = require ("./temp_posts.json");
+
+export default class Feed extends Component {
+
+    renderItem =({ item: post }) => {
+        return <PostCard post={post} />;
+    };
+
+    keyExtractor = (item, index) => index.toSpring();
+
+    render(){
+        return (
+            <View style={styles.container}>
+             <SafeAreaView style={styles.droidSafeArea} />
+             <View style={styles.appTitle}>
+                <Text style={styles.appTitleText}>Espectograma</Text>
+             </View>
+             <View style={styles.cardContainer}>
+                <FlatList
+                keyExtractor={this.keyExtractor}
+                data={posts}
+                renderItem={this.renderItem}
+                />
+             </View>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: "darkBlue"
+    },
+    droidSafeArea : {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight: 35
+    },
+    appTitle: {
+        flex: 0.07,
+        padding: 5,
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontsize: 28
+    },
+    cardContainer: {
+        flex: 0.93
+    }
+});
